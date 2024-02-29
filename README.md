41e0f857479019ce1c3ae2e8e53e9e6d1e24f67f
penja <a.penchuk@scalr.com>
2024-02-29 11:29:06 +0200

    add new structure

--- README.md
+++
@@ -1,2 +0,0 @@
-# terraform-scalr-templates
-The list of useful templates manage https://scalr.io/ by terraform 
---
+++ flat-with-tfvars/main.tf
@@ -0,0 +1,11 @@
+variable "foo" {}
+
+resource null_resource "always"{
+  triggers = {
+    always_run = "${timestamp()}"
+  }
+
+  provisioner "local-exec" {
+    command = "echo ${var.foo}"
+  }
+}
